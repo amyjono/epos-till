398ebe3b2a614b832cde71656d4fa195c897b9b3
Amy Johnson <amylouise_johnson@hotmail.comgit add index.html>
2018-05-31 23:08:53 +0100

    updated epos till

--- MainController.js
+++ MainController.js
@@ -14,30 +14,55 @@
     self.productURI = 'https://api.eposnowhq.com/api/V2/product/';
 
     //Define API key
-    self.API_KEY = "MTZWREhQTlpJV1BQTEVGM0U2N1cxQlJEQ1lWMDcwWkU6SzVMVFVUWVFSMllKMjlJUDROUUxMTzFORUhGNzlIT0c="
+    self.API_KEY = "MTZWREhQTlpJV1BQTEVGM0U2N1cxQlJEQ1lWMDcwWkU6SzVMVFVUWVFSMllKMjlJUDROUUxMTzFORUhGNzlIT0c=";
 
     // Get the response, log it in console to test
     var onApiComplete = function(response) {
       console.log(response);
-      $scope.hotDrinksData = (response.data);
-  };
+      $scope.apiData = (response.data);
+    };
 
     // GET API and then assign the response    
     $http({
       method: 'GET',
-      url: self.productURI + "?categoryID=1086251",
+      url: self.productURI,
       headers: {
         'Authorization': 'Basic ' + self.API_KEY
       },
-
     }).then(onApiComplete);
 
-    /* $scope.list = document.getElementsByClassName("eat-in").addEventListener(click, addToList);
+    // Empty array for list of items
+    $scope.itemList = [];
+
+    // Empty array to log the price seperately
+    var price = [];
+
+    // Empty array for totalPrice to be displayed
+    $scope.totalPrice = 0;
+
+    // ng-click function to display Name and SalePrice in .total div
+    $scope.addItem = function(item) {
+      $scope.itemList.push(item.Name + " " + item.SalePrice);
+    };
+
+    // Equation for totalPrice
+    function getSum(total, num) {
+      return (total + num);
+    }
+
+    // Get the totalPrice and display on screen, parseFloat to return floating point number
+
+    $scope.addPrices = function(item) {
+      price.push(item.SalePrice);
+      $scope.totalPrice = parseFloat(price.reduce(getSum)).toFixed(2);
+    };
     
-    function addToList() {
-    document.getElementById("list").innerHTML = ..
-    }; */
-    
+    // Function to clear order
+    $scope.clearOrder = function(){
+      $scope.itemList = [];
+      $scope.totalPrice = 0;
+    };
+
   };
   app.controller("MainController", ["$scope", "$http", MainController]);
 
--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-(function(){
+(function() {
 
-    var app = angular.module("eposTill", ["ngRoute"]); //define module and dependencies
-    
-    app.config(function($routeProvider){ 
-      $routeProvider
-        .when("/main", {
-          templateUrl: "main.html",
-          controller: "MainController"
-        })
-        .otherwise({redirectTo:"/main"});
-    });
-    
+  var app = angular.module("eposTill", ["ngRoute"]); //define module and dependencies
+
+  app.config(function($routeProvider) {
+    $routeProvider
+      .when("/main", {
+        templateUrl: "main.html",
+        controller: "MainController"
+      })
+  .otherwise({
+    redirectTo: "/main"
+  });
+});
+
 }());
